616e5085e5d2240eb3497face82974960f049bf3
Muslima <mkh00@yandex.ru>
2021-01-05 23:19:54 +0500

    update to do list

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
 $('button.btn.remove').on('click', function () {
     $(this).parents('li.list-group-item').remove()
-
 })
-$('a.btn.btn-primary').on('click', function (e) {
-
-    if ($("input[type='text']") != "") {
-        $('.list').append(`<li class="list-group-item">${$("input[type='text']").val()}<button class = "btn remove">X</button></li>`)
+let addToDo = $("input[type='text']");
+addToDo.keydown(function (e) {
+    if (e.keyCode == 13) {
+        if (addToDo.val() != "") {
+            $('.list').append(`<li class="list-group-item">${addToDo.val()}<button class = "btn remove">X</button></li>`)
+            $("input[type='text']").val("")
+        }
+    }
+    $('button.btn.remove').on('click', function () {
+        $(this).parents('li.list-group-item').remove()
+    })
+});
+$('a.btn.btn-primary').on('click', function () {
+    if (addToDo.val() != "") {
+        $('.list').append(`<li class="list-group-item">${addToDo.val()}<button class = "btn remove">X</button></li>`)
         $("input[type='text']").val("")
     }
-   
     $('button.btn.remove').on('click', function () {
         $(this).parents('li.list-group-item').remove()
     })
